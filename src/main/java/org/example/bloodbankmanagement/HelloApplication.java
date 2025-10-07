36fda769649818a5895392f63d72d6063e9ecdcb
ichrakjaifra <ichrak.jaifra@gmail>
2025-10-07 14:24:38 +0100

    Initial commit with full project

---
+++ src/main/java/org/example/bloodbankmanagement/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.bloodbankmanagement;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
